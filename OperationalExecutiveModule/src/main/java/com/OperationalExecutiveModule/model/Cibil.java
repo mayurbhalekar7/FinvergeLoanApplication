b8a2ad4bdb503fcaac559c0b27e358aa64fddaa0
Akshay221193 <akshay.jaiswal2211@gmail.com>
2024-08-05 15:44:49 +0530

    New Module Created Operational Excecative By Akshay

---
+++ OperationalExecutiveModule/src/main/java/com/OperationalExecutiveModule/model/Cibil.java
@@ -0,0 +1,27 @@
+package com.OperationalExecutiveModule.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Cibil {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int cibilId;
+	
+	private int cibilScore;
+	
+	private Date cibilDate;
+	
+	private  String cibilStatus;
+	
+	
+
+}
